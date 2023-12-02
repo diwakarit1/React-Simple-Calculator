d5a4051e7c5c59afe6d6186ff9bd1ba807d59e69
diwakarit1 <diwakarbabuit1@gmail.com>
2023-12-02 12:50:56 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+/>;
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
